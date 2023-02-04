f1cffea4b9e89f650f4863e9967118f088f87ba6
Rafael Aragao morais <ram.eng19@uea.edu.br>
2023-02-04 10:03:40 -0400

    Creation of main menu with options
    - Main menu was created with some options to handle vsync, resolution and full screen modifications.
    (These options are for user adjustments!)
    
    - added a new scene called MainGame
    - Renamed the Sample scene to MainMenu
    The files that was renamed is to organization proposes.

---
+++ Assets/SolutionAndUXTest/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MenuController : MonoBehaviour
+
+{
+    private GameObject _optionsPanel;
+    private CanvasGroup _optionsGroup;
+
+    private void Start()
+    {
+        _optionsPanel = GameObject.FindWithTag("OptionsPanel");
+        _optionsGroup = _optionsPanel.GetComponent<CanvasGroup>();
+    }
+
+    public void CloseOptions()
+    {
+        _optionsGroup.alpha = 0;
+        _optionsGroup.interactable = false;
+        _optionsGroup.blocksRaycasts = false;
+    }
+
+    public void OpenOptions()
+    {
+        _optionsGroup.alpha = 1;
+        _optionsGroup.interactable = true;
+        _optionsGroup.blocksRaycasts = true;
+    }
+}
