765508eabdcd899373e991679e28d2309adccd4a
Rafael Aragao morais <ram.eng19@uea.edu.br>
2023-02-05 10:34:00 -0400

    Improve menu options
    - Improve menu options to set resolutions, vsync and full screen properly

--- Assets/SolutionAndUXTest/Scripts/MenuController.cs
+++ Assets/SolutionAndUXTest/Scripts/MenuController.cs
@@ -1,15 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 
 {
     private GameObject _optionsPanel;
     private CanvasGroup _optionsGroup;
-
+    private Resolution[] _screenResolutions;
+    private TMP_Dropdown _resolutionDropDown;
+    private List<string> _resolutionOptionsStr;
+     
     private void Start()
     {
+        _resolutionOptionsStr = new List<string>();
         _optionsPanel = GameObject.FindWithTag("OptionsPanel");
         _optionsGroup = _optionsPanel.GetComponent<CanvasGroup>();
+        _screenResolutions = Screen.resolutions;
+        _resolutionDropDown = GameObject.FindWithTag("ResolutionDropDown").GetComponent<TMP_Dropdown>();
+        _resolutionDropDown.ClearOptions();
+        FeedDropDownWithScreenResolutions(_screenResolutions);
     }
 
     public void CloseOptions()
@@ -24,5 +36,28 @@ public class MenuController : MonoBehaviour
         _optionsGroup.alpha = 1;
         _optionsGroup.interactable = true;
         _optionsGroup.blocksRaycasts = true;
+    }
+
+    public void SetFullScreen(bool fullScreen)
+    {
+        Screen.fullScreen = fullScreen;
+    }
+    
+    public void SetVsync(bool Vsync)
+    {
+        QualitySettings.vSyncCount = Vsync ? 1 : 0;
+    }
+
+    private void FeedDropDownWithScreenResolutions(Resolution[] resolutions)
+    {
+        _resolutionOptionsStr.Clear();
+        foreach (var resolution in resolutions)
+        {
+            string optionName = resolution.width + "x" + resolution.height;
+            _resolutionOptionsStr.Add(optionName);
+        }
+        
+        _resolutionDropDown.AddOptions(_resolutionOptionsStr);
+        
     }
 }
